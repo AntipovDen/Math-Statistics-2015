e7ac6a1982b66285495bbf6d1f81620fef56c093
AntipovDen <antipovden@yandex.ru>
2015-05-21 19:10:57 +0300

    indian method

--- my_method.py
+++ my_method.py
@@ -2,6 +2,7 @@ __author__ = 'Den'
 
 from random import random, randint
 from numpy import sum
+from math import sqrt, log
 
 n = 10
 l = 100
@@ -35,18 +36,23 @@ def getDistributions(x, f):
 
     f1 = [0] * n
     f2 = [0] * n
-    for i in range(len(x)):
-        if f[i] == 0:
-            f1[x[i]] += 1
-        else:
-            f2[x[i]] += 1
+    for i in range(l):
+            if f[i] == 0:
+                f1[x[i]] += 1
+            else:
+                f2[x[i]] += 1
+    for i in range(n):
+        if f1[i] == 0:
+            f1[i] += 0.5
+        if f2[i] == 0:
+            f2[i] += 0.5
     sum_f1 = sum(f1)
     sum_f2 = sum(f2)
     f1 = [i / sum_f1 for i in f1]
     f2 = [i / sum_f2 for i in f2]
     return f1, f2
 
-#functions for my mthod
+#functions for my method
 def getFirstDeckProbability(f):
     return 1 - sum(f)/len(f)
 
@@ -72,29 +78,75 @@ def analizeExperimentMyMethod(x, f):
 #functions for some indian method
 def bhattacharyyaDistance(distributions):
     f1, f2 = distributions
+    s = sum([sqrt(f1[i] * f2[i]) for i in range(n)])
+    if s == 0:
+        return -float('infinity')
+    return log(s)
 
+##Evolution
+populationSize = 100
+newGenSize = 80
+
+def generateIndividual():
+    x =[]
+    cur = 1
+    for i in range(l):
+        if random() < switch:
+            cur = 1 - cur
+        x.append(cur)
+    return x
+
+def selectParents(population, f):
+    values = [f(p) for p in population]
+    for i in range(1, populationSize):
+        values[i] += values[i - 1]
+    values = [i/ values[-1] for i in values]
+    r = random()
+    par1 = population[min([k for k in range(populationSize) if values[k] > r])]
+    r = random()
+    par2 = population[min([k for k in range(populationSize) if values[k] > r])]
+    return par1, par2
+
+def crossingover(parents):
+    par1, par2 = parents
+    i = randint(0, l - 1)
+    return par1[:i] + par2[i:], par2[:i] + par1[i:]
 
 def evolutionaryMinimizer(f, length):
+    population = [generateIndividual() for i in range(populationSize)]
+    for i in range(1000):
+        if i % 10 == 0:
+            print(i)
+        new_generation = []
+        for i in range(newGenSize // 2):
+            c1, c2 = crossingover(selectParents(population, f))
+            new_generation += [c1, c2]
+        tmp = [(f(population[i]), population[i]) for i in range(populationSize)]
+        tmp.sort()
+        population = [t[1] for t in tmp[:(populationSize - newGenSize)]] + new_generation
+    return min([(f(population[i]), population[i]) for i in range(populationSize)])
+
+def mutationMinimizer(f, length):
     iterations_without_changes = 0
     x = [randint(0, 1) for i in range(length)]
     current_value = f(x)
     while iterations_without_changes < 1000:
-        # x1 = []
-        # cur_source = 1
-        # for i in range(length):
-        #     if random() < switch:
-        #         cur_source = 1 - cur_source
-        #     x1.append(cur_source)
-        i = randint(0, length - 1)
-        x[i] = 1 - x[i]
+        x1 = []
+        cur_source = 1
+        for i in range(length):
+            if random() < switch:
+                cur_source = 1 - cur_source
+            x1.append(cur_source)
+        # i = randint(0, length - 1)
+        # x[i] = 1 - x[i]
 
         new_value = f(x)
         if new_value <= current_value:
             current_value = new_value
-            # x = x1
+            x = x1
             iterations_without_changes = 0
         else:
-            x[i] = 1 - x[i]
+            # x[i] = 1 - x[i]
             iterations_without_changes += 1
     return x
 
@@ -107,9 +159,15 @@ print(f)
 def vectorResult(vector):
     return analizeExperimentMyMethod(x, vector)
 
-result = evolutionaryMinimizer(vectorResult, l)
-print("result:", analizeExperimentMyMethod(x, result), "real distribution:", analizeExperimentMyMethod(x, f))
+def vectorResultBhattacharyya(vector):
+    return bhattacharyyaDistance(getDistributions(x, vector))
+
+
+result = evolutionaryMinimizer(vectorResultBhattacharyya, l)[1]
 print(result)
 print(f)
+
+print("result:", bhattacharyyaDistance(getDistributions(x, result)),
+      "real distribution:", bhattacharyyaDistance(getDistributions(x, f)))
 # f = [randint(0, 1) for i in range(l)]
 # print(analizeExperimentMyMethod(x, f))
